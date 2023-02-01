1d632b5d4b0f177cc9551e2adb37ca7eb86f90c3
Yahia <100961050+YahiaXXX@users.noreply.github.com>
2023-02-01 12:22:26 +0100

    eight commit

--- src/components/Regex.js
+++ src/components/Regex.js
@@ -3,4 +3,4 @@ export const validEmail = new RegExp(
  );
  export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
  export const validLink = new RegExp('/^(?!:\/\/)([a-zA-Z0-9-]+\.){1,5}[a-zA-Z0-9-][a-zA-Z0-9-]+\.[a-zA-Z]{2,64}?$/i');
- export const validIp = new RegExp('/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/')
+ export const validIp = new RegExp('^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
